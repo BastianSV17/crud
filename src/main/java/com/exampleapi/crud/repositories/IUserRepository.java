bb316eb911dc979c380c8dd4e4d48f286d3a6731
Joann Vasquez <129774143+BastianSV17@users.noreply.github.com>
2024-03-16 00:36:18 -0400

    API base code.

---
+++ src/main/java/com/exampleapi/crud/repositories/IUserRepository.java
@@ -0,0 +1,10 @@
+package com.exampleapi.crud.repositories;
+
+import com.exampleapi.crud.models.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IUserRepository extends JpaRepository<UserModel, String> {
+
+}
