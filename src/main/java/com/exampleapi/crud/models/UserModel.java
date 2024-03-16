1af0147aae8ead4051a2fc431583c1975ef133e6
Joann Vasquez <129774143+BastianSV17@users.noreply.github.com>
2024-03-16 14:38:42 -0400

    Simplified UserModel class using Lombok.

--- src/main/java/com/exampleapi/crud/models/UserModel.java
+++ src/main/java/com/exampleapi/crud/models/UserModel.java
@@ -3,13 +3,15 @@ package com.exampleapi.crud.models;
 import java.util.UUID;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "users")
+@Getter
+@Setter
 public class UserModel {
 
 	@Id
@@ -26,37 +28,5 @@ public class UserModel {
 
 	public UserModel() {
 		id = UUID.randomUUID().toString();
-	}
-
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getLogin() {
-		return login;
-	}
-
-	public void setLogin(String email) {
-		this.login = email;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
 	}
 }
